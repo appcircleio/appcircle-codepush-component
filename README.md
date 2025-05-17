8dc6cfdc3c311b8979921e4a1e81f9d11d515c6a
boztopuz <boztopuz@gmail.com>
2025-05-17 12:55:08 +0300

    [BE-6053]: Fixed feedbacks

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ Appcircle CodePush component lets you deliver over-the-air updates to your React
 ## Required Inputs
 
 - `AC_REPOSITORY_DIR`: Relative path of the React Native project.
-- `AC_CODE_PUSH_TOKEN`: Appcircle personal access token. You can create a new token in the Appcircle dashboard under `Settings > Security > Personal Access Token`. For details, see [Generating and Managing Personal API Tokens](/appcircle-api-and-cli/api-authentication#generatingmanaging-the-personal-api-tokens).
+- `AC_CODE_PUSH_TOKEN`: Appcircle personal access token. You can create a new token in the Appcircle dashboard under `Settings > Security > Personal Access Token`. For details, see [Generating and Managing Personal API Tokens](https://docs.appcircle.io/appcircle-api-and-cli/api-authentication#generatingmanaging-the-personal-api-tokens).
 - `AC_CODE_PUSH_APP_NAME`: This parameter specifies the name of the app in Appcircle. For example `MyApp-Android` or `MyApp-iOS`.
 - `AC_CODE_PUSH_DEPLOYMENT_NAME`: This parameter specifies which deployment you want to release the update to. For example `Staging`.
 - `AC_CODE_PUSH_TARGET_BINARY_VERSION`: This parameter specifies the target binary version of the app that this update is intended for. It should be in the format `1.0.0`.
 
 ## Optional Inputs
 
+- `AC_CODE_PUSH_SERVER_URL`: This parameter specifies the server URL used by self-hosted Appcircle instances to authenticate CLI access. Ignore this if you are not a self-hosted Appcircle user.
+- `AC_CODE_PUSH_AUTH_URL`: This parameter specifies the authentication URL used by self-hosted Appcircle instances to authenticate CLI access. Ignore this if you are not a self-hosted Appcircle user.
 - `AC_CODE_PUSH_DESCRIPTION`: This parameter provides an optional changelog for the deployment.
 - `AC_CODE_PUSH_ROLLOUT_PERCENTAGE`: This parameter specifies the percentage of users (as an integer between 1 and 100) that should be eligible to receive this update.
 - `AC_CODE_PUSH_EXTRA_ARGUMENTS`: Extra command line arguments for Appcircle CodePush CLI command. For example, add `--debug` for verbose logs.
--- main.rb
+++ main.rb
@@ -14,11 +14,12 @@ app_name = env_has_key('AC_CODE_PUSH_APP_NAME')
 deployment_name = env_has_key('AC_CODE_PUSH_DEPLOYMENT_NAME')
 platform_type = %w[apk aab].include?(ENV['AC_OUTPUT_TYPE']&.downcase) ? 'android' : 'ios'
 target_version = env_has_key('AC_CODE_PUSH_TARGET_BINARY_VERSION')
-server_url = get_env_variable('AC_CODE_PUSH_SERVER_URL') || "https://dev-api.appcircle.io/codepush"
-auth_url = get_env_variable('AC_CODE_PUSH_AUTH_URL') || "https://dev-auth.appcircle.io"
+server_url = get_env_variable('AC_CODE_PUSH_SERVER_URL')
+auth_url = get_env_variable('AC_CODE_PUSH_AUTH_URL')
 description = get_env_variable('AC_CODE_PUSH_DESCRIPTION')
 rollout_percentage = get_env_variable('AC_CODE_PUSH_ROLLOUT_PERCENTAGE')
 extra_arguments = get_env_variable('AC_CODE_PUSH_EXTRA_ARGUMENTS')
+code_push = "appcircle-code-push"
 
 
 def run_command(cmd)
@@ -46,18 +47,21 @@ end
 run_command("sudo npm install -g @appcircle/codepush-cli")
 
 if !server_url.to_s.empty? && !auth_url.to_s.empty?
-  login_cmd = "appcircle-code-push login --accessKey #{access_token} --serverUrl #{server_url} --authUrl #{auth_url}"
+  login_cmd = "#{code_push} login --accessKey #{access_token} --serverUrl #{server_url} --authUrl #{auth_url}"
 else
-  login_cmd = "appcircle-code-push login --accessKey #{access_token}"
+  login_cmd = "#{code_push} login --accessKey #{access_token}"
 end
 
 run_command("#{login_cmd}")
 
-cmd = "appcircle-code-push release-react #{app_name} #{platform_type} --deploymentName #{deployment_name} --targetBinaryVersion #{target_version}"
-cmd += " --description '#{description}'" if description
-cmd += " --rollout #{rollout_percentage}" if rollout_percentage
-cmd += " #{extra_arguments}" if extra_arguments
-
-Dir.chdir(project_path) do 
-    run_command(cmd)
+release_cmd = [
+  "#{code_push} release-react #{app_name} #{platform_type}",
+  "--deploymentName #{deployment_name}",
+  "--targetBinaryVersion #{target_version}"
+]
+release_cmd << "--description '#{description}'" if description
+release_cmd << "--rollout #{rollout_percentage}" if rollout_percentage
+release_cmd << extra_arguments if extra_arguments
+Dir.chdir(project_path) do
+  run_command(release_cmd.join(' '))
 end
